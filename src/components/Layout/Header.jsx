ea866470cb2cf2ade5bbaa23499ec403f6cf15e8
Jonathan Bello <jonathan.bellofg@gmail.com>
2023-08-23 16:04:10 -0600

    terminado maquetado el header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,29 +1,46 @@
-import React from "react"
+import React, { useEffect, useState } from 'react'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import useWindowSize from '../../hooks/useWindowSize'
+import HeaderNav from './HeaderNav'
 
-import { RxHamburgerMenu } from "react-icons/rx"
-import LogoHorizontal from "../../assets/images/LogoHorizontal.svg"
-
-import iconFacebook from "../../assets/icons/facebook.svg"
-import iconInstagram from "../../assets/icons/instagram.svg"
-import iconiLinkedin from "../../assets/icons/linkedin.svg"
+import LogoHorizontal from '../../assets/images/LogoHorizontal.svg'
+import iconFacebook from '../../assets/icons/facebook.svg'
+import iconInstagram from '../../assets/icons/instagram.svg'
+import iconiLinkedin from '../../assets/icons/linkedin.svg'
 
 const Header = () => {
+  const windowSize = useWindowSize()
+  const [showMenu, setShowMenu] = useState(false)
+
+  useEffect(() => {
+    if (windowSize.width > 768) {
+      setShowMenu(true)
+    }
+  }, [windowSize])
+
   return (
-    <header className="Header ed-grid s-grid-3 full s-cross-center gap-0">
-      <RxHamburgerMenu className="Header__menu" />
+    <>
+      <header className="Header ed-grid s-grid-3 m-grid-2 full s-cross-center gap-0">
+        <RxHamburgerMenu
+          className="Header__menu"
+          onClick={() => setShowMenu(!showMenu)}
+        />
 
-      <img
-        className="Header__logo s-to-center"
-        src={LogoHorizontal}
-        alt="Ironman Capital"
-      />
+        <img
+          className="Header__logo"
+          src={LogoHorizontal}
+          alt="Ironman Capital"
+        />
 
-      <div className="Header__buttons">
-        <img className="Header__button" src={iconFacebook} alt="Facebook" />
-        <img className="Header__button" src={iconInstagram} alt="Instagram" />
-        <img className="Header__button" src={iconiLinkedin} alt="Linkedin" />
-      </div>
-    </header>
+        <div className="Header__buttons">
+          <img className="Header__button" src={iconFacebook} alt="Facebook" />
+          <img className="Header__button" src={iconInstagram} alt="Instagram" />
+          <img className="Header__button" src={iconiLinkedin} alt="Linkedin" />
+        </div>
+      </header>
+
+      {showMenu && <HeaderNav showMenu={showMenu} setShowMenu={setShowMenu} />}
+    </>
   )
 }
 
---
+++ src/components/Layout/HeaderNav.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import LogoHorizontal from '../../assets/images/LogoHorizontal.svg'
+import { RxCross2 } from 'react-icons/rx'
+
+const HeaderNav = ({ showMenu, setShowMenu }) => {
+  return (
+    <nav className={`HeaderNav ${showMenu ? 'HeaderNav--active' : ''}`}>
+      <img
+        className="HeaderNav__Logo"
+        src={LogoHorizontal}
+        alt="Ironman Capital"
+      />
+
+      <RxCross2
+        className="HeaderNav__Icon"
+        onClick={() => setShowMenu(!showMenu)}
+      />
+
+      <div className="HeaderNav__Links">
+        <Link to="/">Home</Link>
+        <Link to="/WhoWeAre">Who we are</Link>
+        <Link to="/Partners">Ironman Partners</Link>
+        <Link to="/Portafolio">Portafolio</Link>
+        <Link to="/Contact">Contact</Link>
+      </div>
+
+      <select className="HeaderNav__Select">
+        <option value="es">ES</option>
+        <option value="en">EN</option>
+      </select>
+    </nav>
+  )
+}
+
+export default HeaderNav
